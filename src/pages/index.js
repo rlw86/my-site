e7e7e9165b6e915bba5d074dceb4f2759e4b1439
Russell <russellwyatt@Russells-iMac.local>
2018-12-19 11:54:39 -0500

    Just syncing the folders

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Russell Wyatt</h1>
+    <p>Hello, I'm a designer who focuses on combining human-centered design, creative direction and storytelling to create engaging brands, products, and digital experiences.</p>
+    <p>Let's build something together!</p>
+    <p>I just want to use git with my project seamlessly</p>
+    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
