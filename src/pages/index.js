a2ea5356cabe2ffe475234cd18e660499233e9eb
Russell <russell@weareunleashed.co>
2018-12-20 09:43:17 -0500

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,13 @@ import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Russell Wyatt</h1>
-    <p>Hello, I'm a designer who focuses on combining human-centered design, creative direction and storytelling to create engaging brands, products, and digital experiences.</p>
-    <p>Let's build something together!</p>
-    <p>I just want to use git with my project seamlessly</p>
-    <blockquote>I am looking forward to experimenting with these new technologies!</blockquote>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
+  <div className="hero">
+    <div className="HeroGroup">
+      <h1> Russell Wyatt </h1>
+        <p> Hello, I 'm a designer who focuses on combining human-centered design, creative direction and storytelling to create engaging brands, products, and digital experiences.</p>
+        <Link to="/page-2/"> Go to page 2 </Link>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+  </div>
   </Layout>
 )
 
